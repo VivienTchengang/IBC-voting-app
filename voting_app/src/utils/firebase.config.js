f424cdf5e7db7c4a3c4c28b5170e32748b9509c1
NML124 <nehemielwatumba@gmail.com>
2024-12-12 11:15:16 +0530

    Init Firebase Auth and Firestore

---
+++ voting_app/src/utils/firebase.config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth} from 'firebase/auth'
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
+};
+
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth =getAuth(app)
+
+export {app,auth}
